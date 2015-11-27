786e0b681cfd1017e597b05e65f2b1a5fe7949e8
rosickyhyman <rosicky.hyman@gmail.com>
2015-11-27 22:45:59 +0800

    sencond commit.base frame of game finished

--- src/com/hyman/plantvszombies/engine/GameCotroller.java
+++ src/com/hyman/plantvszombies/engine/GameCotroller.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.cocos2d.layers.CCTMXTiledMap;
 
 import com.hyman.plantvszombies.bean.ShowPlant;
-
+/*
+ * 
+ * 处理游戏开始后的工作
+ */
 public class GameCotroller {
 
 	public GameCotroller() {
